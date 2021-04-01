3427d1339bc40295d9eca3e081873fe269b2d44c
Anna <hasselstromanna@gmail.com>
2021-04-01 15:23:47 +0200

    done

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import React from 'react';
 import { ColorList } from './ColorList.js';
 import { AddColorForm } from 'AddColorForm';
 
-import { Checkbox } from './sandboxHooks/hooks';
 
 export const App = () => {
 
   return (
     <>
-      <Checkbox />
       <AddColorForm />
       <ColorList />
     </>
